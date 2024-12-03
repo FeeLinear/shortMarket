165e165d8e445b11539a18dae579a8d92dbb6b5b
Feelinear <444730413@qq.com>
2024-12-03 18:06:18 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,133 @@
+
+    /** 
+     * upLimitsNum: 当日涨停数
+     * dropLimitsNum: 当日跌停数
+     * firstBoardNum: 当日首板
+     * firstBoomNum: 首板炸板数
+     * secondBoardNum: 当日二板
+     * secondBoomNum: 当日二板炸板数
+     * thirdBoardNum: 当日三板
+     * thirdBoomNum: 当日三板炸板数
+     * upFourBoardNum: 高度板
+     * upFourBoardRate: 高度板连扳率
+     * boomRate: 炸板率
+     * yesterdayUpTodayRise: 昨日涨停今表现
+     * yesterdayLineTodayRise: 昨日连扳今表现
+     * yesterdayBoomTodayRise: 昨日炸板今表现
+     * 
+     * 
+    */
+    var allData = {
+      '20241125': {
+        upLimitsNum: 157,
+        dropLimitsNum: 15,
+        firstBoardNum: 110,
+        firstBoomNum: 23,
+        secondBoardNum: 23,
+        secondBoomNum: 1,
+        thirdBoardNum: 7,
+        thirdBoomNum: 0,
+        upFourBoardNum: 17,
+        upFourBoardRate: '68',
+        boomRate: '13.47',
+        yesterdayUpTodayRise: '5.68',
+        yesterdayLineTodayRise: '5.07',
+        yesterdayBoomTodayRise: '1.54'
+      },
+      '20241126': {
+        upLimitsNum: 92,
+        dropLimitsNum: 55,
+        firstBoardNum: 61,
+        firstBoomNum: 40,
+        secondBoardNum: 18,
+        secondBoomNum: 11,
+        thirdBoardNum: 3,
+        thirdBoomNum: 6,
+        upFourBoardNum: 9,
+        upFourBoardRate: '37',
+        boomRate: '38.60',
+        yesterdayUpTodayRise: '-1.15',
+        yesterdayLineTodayRise: '-3.57',
+        yesterdayBoomTodayRise: '-5.30'
+      },
+      '20241127': {
+        upLimitsNum: 103,
+        dropLimitsNum: 15,
+        firstBoardNum: 82,
+        firstBoomNum: 14,
+        secondBoardNum: 11,
+        secondBoomNum: 4,
+        thirdBoardNum: 3,
+        thirdBoomNum: 3,
+        upFourBoardNum: 7,
+        upFourBoardRate: '58',
+        boomRate: '18.25',
+        yesterdayUpTodayRise: '1.67',
+        yesterdayLineTodayRise: '1.45',
+        yesterdayBoomTodayRise: '0.88'
+      },
+      '20241128': {
+        upLimitsNum: 95,
+        dropLimitsNum: 6,
+        firstBoardNum: 63,
+        firstBoomNum: 42,
+        secondBoardNum: 19,
+        secondBoomNum: 5,
+        thirdBoardNum: 7,
+        thirdBoomNum: 2,
+        upFourBoardNum: 6,
+        upFourBoardRate: '60',
+        boomRate: '37.36',
+        yesterdayUpTodayRise: '3.64',
+        yesterdayLineTodayRise: '6.62',
+        yesterdayBoomTodayRise: '0.32'
+      },
+      '20241129': {
+        upLimitsNum: 134,
+        dropLimitsNum: 7,
+        firstBoardNum: 100,
+        firstBoomNum: 12,
+        secondBoardNum: 15,
+        secondBoomNum: 5,
+        thirdBoardNum: 8,
+        thirdBoomNum: 1,
+        upFourBoardNum: 11,
+        upFourBoardRate: '84',
+        boomRate: '17.86',
+        yesterdayUpTodayRise: '3.10',
+        yesterdayLineTodayRise: '5.19',
+        yesterdayBoomTodayRise: '-1.42'
+      },
+      '20241202': {
+        upLimitsNum: 169,
+        dropLimitsNum: 5,
+        firstBoardNum: 115,
+        firstBoomNum: 36,
+        secondBoardNum: 31,
+        secondBoomNum: 15,
+        thirdBoardNum: 8,
+        thirdBoomNum: 2,
+        upFourBoardNum: 15,
+        upFourBoardRate: '79',
+        boomRate: '24.27',
+        yesterdayUpTodayRise: '5.48',
+        yesterdayLineTodayRise: '7.17',
+        yesterdayBoomTodayRise: '0.00'
+      },
+      '20241203': {
+        upLimitsNum: 119,
+        dropLimitsNum: 6,
+        firstBoardNum: 72,
+        firstBoomNum: 27,
+        secondBoardNum: 22,
+        secondBoomNum: 10,
+        thirdBoardNum: 13,
+        thirdBoomNum: 4,
+        upFourBoardNum: 12,
+        upFourBoardRate: '52',
+        boomRate: '29.95',
+        yesterdayUpTodayRise: '2.26',
+        yesterdayLineTodayRise: '2.57',
+        yesterdayBoomTodayRise: '-2.16'
+      }
+    }
